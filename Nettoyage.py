ec9840e35f25e9113646c0e460c66b71a15d1267
Micka98 <Micka98>
2023-11-08 17:28:07 +0000

    Second commit

--- Nettoyage.py
+++ Nettoyage.py
@@ -15,8 +15,8 @@ null_counts = df.select([sum(col(c).isNull().cast("int")).alias(c) for c in df.c
 # Afficher le résultat
 null_counts.show()
 
-
 # COMMAND ----------
+
 
 # Extraire les noms de colonnes et leurs comptages de valeurs nulles
 null_counts = null_counts.collect()[0]
@@ -25,15 +25,10 @@ null_counts = null_counts.collect()[0]
 for column_name, null_count in null_counts.asDict().items():
     print(f"Colonne : {column_name}, Nombre de valeurs nulles : {null_count}")
 
-
-# COMMAND ----------
-
-
-
 # COMMAND ----------
 
 # Filtrez les colonnes ayant moins de 400 valeurs nulles
-selected_columns = [column_name for column_name, null_count in null_counts.asDict().items() if null_count < 200]
+selected_columns = [column_name for column_name, null_count in null_counts.asDict().items() if null_count < 1500000]
 
 # Créez un nouveau DataFrame avec les colonnes sélectionnées
 filtered_df = df.select(selected_columns)
@@ -44,3 +39,20 @@ filtered_df.printSchema()
 # Affichez les premières lignes du nouveau DataFrame
 filtered_df.display()
 
+# COMMAND ----------
+
+# Supprimer les colonnes "colonne1" et "colonne2"
+columns_to_drop = ["Code_postal_(brut)", "Code_INSEE_(BAN)", "N°_département_(BAN)"]
+filtered_df = filtered_df.drop(*columns_to_drop)
+filtered_df.display()
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+ 
+
+filtered_df.write.mode('overwrite').saveAsTable('lenomdetatable') 
